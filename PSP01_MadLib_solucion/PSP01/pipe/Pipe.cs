56b3a82ebe84b4c701418eff022e9a82f1e1920b
Maitane Ibero <mibero@birt.eus>
2022-05-31 08:44:51 +0200

    Ejercicio

---
+++ PSP01_MadLib_solucion/PSP01/pipe/Pipe.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+
+namespace pipe
+{
+    class Pipe
+    {
+        //Método Main
+        //Descripción: Se encarga de abrir un proceso con el servidor, abrir un pipe con el servidor para su posterior comunicación,
+        //genera unos buffer de comunicación y gestiona la lectura y escritura de unos cuentos MadLib. Es la parte cliente del programa.
+        //Sobre todo se encarga de la comunicación con el usuario.
+        static void Main(string[] args)
+        {
+
+            //Lanza el proceso servidor para la comunicación con el mismo.
+            Process p;
+            StartServer(out p);
+            Task.Delay(1000).Wait();
+            string operacion;
+            try
+            {
+
+                //Client
+                //Creación de conexión con Pipe y creación de buffer para la comunicación.
+                var client = new NamedPipeClientStream("PSP01_UD01_Pipes");
+                client.Connect();
+                Console.WriteLine("Estableciendo conexión con el servidor");
+                StreamReader reader = new StreamReader(client);
+                StreamWriter writer = new StreamWriter(client);
+
+                while (true)
+                {
+                    String palabra = String.Empty;
+                    operacion = String.Empty;
+                    bool lecturafichero = false;
+
+                    do
+                    {
+                        //Se solicita nombre del cuento.
+                        operacion = SolicitarNombreDelCuento();
+
+                        //Gestión de si el fichero es válido o no.
+                        Console.WriteLine("Tubo Cliente procesando datos: '{0}'", operacion);
+                        writer.WriteLine(operacion);
+                        writer.Flush();
+                        operacion = reader.ReadLine();
+
+                        if (operacion.Contains("Ficheros abierto."))
+                        {
+                            lecturafichero = true;
+
+                        }
+                        else
+                        {
+                            lecturafichero = false;
+
+                        }
+                        operacion = String.Empty;
+                    } while (lecturafichero == false);
+
+
+                    //Procesamiendo de los datos entre usuario y servidor
+                    operacion= ProcesaDatos(writer, reader);
+
+                    //Muestra el contenido del cuento por consola.
+                    MostrarCuento(operacion,reader);
+
+                }
+
+                //Cierre de los buffer
+                reader.Close();
+                writer.Close();
+
+                //Parar pipes
+                if (p != null && !p.HasExited)
+                {
+                    p.Kill();
+                    p = null;
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}  Apangado servidor por error", e.Message);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  Apangado servidor por error", e.Message);
+                Console.ReadLine();
+            }
+        }
+
+
+        //Método  StartServer
+        //Lanza el proceso servidor 
+        //@ Process p1: Se devuelve como parámetro el proceso lanzado.
+        static void StartServer(out Process p1)
+        {
+            // iniciar un proceso con el servidor y devolver
+            ProcessStartInfo info = new ProcessStartInfo(@"..\..\..\..\pipeServidor\bin\Release\netcoreapp3.1\pipeServidor.exe");
+            // su valor por defecto el false, si se establece a true no se "crea" ventana
+            info.CreateNoWindow = false;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            // indica si se utiliza el cmd para lanzar el proceso
+            info.UseShellExecute = true;
+            p1 = Process.Start(info);
+            
+        }
+
+        //Método  SolicitarNombreDelCuento
+        //Solicita el nombre del cuento por teclado y devuelve un String 
+        
+        private static String SolicitarNombreDelCuento()
+        {
+            Console.WriteLine("\nIndica el nombre del cuento elegido:\n");
+            String nombre = Console.ReadLine();
+            nombre = "N " + nombre;
+            return nombre;
+
+        }
+
+        //Método  SolicitarPalabra
+        //Solicita el nombre del cuento por teclado y devuelve un String
+        //@string operacion: palabra que se quiere enviar al servidor
+        //devuelve un string: La palabra con palabra clave "P" por delante.
+        private static String SolicitarPalabra(string operacion)
+        {
+            var input = String.Empty;
+            Console.WriteLine("{0}:", operacion.Substring(2, operacion.Length - 2));
+            input = Console.ReadLine();
+            input = "P " + input;
+            return input;
+        }
+
+        //Método  EscribirCabecera
+        //Muestra cabecera del cuento
+        private static void EscribirCabecera()
+        {
+            Console.WriteLine();
+            Console.WriteLine("********************\n");
+            Console.WriteLine("El cuento creado es:\n");
+            Console.WriteLine("********************\n");
+        }
+
+
+        //Método  MostrarCuento
+        //Muestra el cuento procesado por consola
+        //@StreamReader read: Lectura del buffer
+        //@string op: String al que se le pasa el contenido del buffer.
+        private static void MostrarCuento(string op, StreamReader read)
+        {
+            
+            EscribirCabecera();
+            do
+            {
+                Console.WriteLine(op);
+                op = read.ReadLine();
+            }while (!string.IsNullOrEmpty(op));
+        }
+
+        //Método  ProcesaDatos
+        //Procesa las palabras de intercambio entre el cliente y servidor.
+        //@StreamReader read: Lectura del buffer
+        //StreamWriter write: Escritura del buffer
+        private static string ProcesaDatos(StreamWriter write, StreamReader read)
+        {
+            String operacion = String.Empty;
+            String palabra = String.Empty;
+            try { 
+                operacion = read.ReadLine();
+                Console.WriteLine("{0}",operacion);
+                while ('P'.Equals(operacion[0]))
+                {
+                     palabra = SolicitarPalabra(operacion);
+                     Console.WriteLine("Tubo Cliente procesando datos: '{0}'", palabra);
+                     write.WriteLine(palabra);
+                                //Console.ReadLine();
+                     write.Flush();
+                     operacion = read.ReadLine();
+                     Console.WriteLine("Tubo Cliente recibiendo datos: '{0}'", operacion);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  Error al procesar datos", e.Message);
+                Console.ReadLine();
+            }
+            return operacion;
+            
+        }
+
+    }
+}
---
+++ PSP01_MadLib_solucion/PSP01/pipeServidor/PipeServidor.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+
+namespace pipeServidor
+{
+    
+    class PipeServidor
+    {
+        private const String FICHEROSALIDA = @"..\..\..\..\cuentos\resultado.txt";
+
+        //Método Main
+        //Descripción: Se encarga de abrir un pipe con el cliente, genera unos buffer de comunicación y gestiona la lectura y escritura de unos cuentos MadLib.
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Establecimiento de conexión con pipe y creación de buffer
+                var server = new NamedPipeServerStream("PSP01_UD01_Pipes");
+                server.WaitForConnection();
+                Console.WriteLine("Conexión a servidor establecida.");
+                Console.WriteLine("Pipe Servidor esperando datos.\n");
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+
+               
+                //Inicialización de objetos
+                StreamReader srfich = StreamReader.Null;
+
+                //Procesamiento de datos
+                //Comunicación con cliente
+                while (true)
+                {
+
+                    //Crea un Stream para la lectura del cuento
+                    srfich = AbrirCuento(reader, writer);
+                    //Procesa las líneas del cuento y lo guarda en un fichero.
+                    ProcesarLinea(srfich, writer,reader, FICHEROSALIDA);
+                    //Envía el contenido del fichero para que lo muestre el cliente por consola.
+                    EnviarCuento(writer, FICHEROSALIDA);
+                }
+
+                //Cierre de todos los Stream
+                srfich.Close();
+                reader.Close();
+                writer.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}  Apangado servidor por error de fichero", e.Message);
+                Console.ReadLine();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  Apangado servidor por error", e.Message);
+                Console.ReadLine();
+            }
+           
+        }
+
+        //Método AbrirCuento
+        //Descripción: Abre un cuento si existe el fichero y sino le envía al cliente para que vuelva a solicitar otro nombre
+        //@StreamReader read: Buffer de lectura del pipe.
+        //@StreamWriter write: Buffer de escritura del pipe.
+        //Devuelve parámetro StreamReader, buffer para manipular el contenido del cuento (fichero).
+
+        private static StreamReader AbrirCuento(StreamReader read, StreamWriter write)
+        {
+            String line = String.Empty;
+            String fichero = String.Empty;
+            StreamReader str = StreamReader.Null;
+            
+            try
+            { 
+                do
+                {
+                    line = read.ReadLine().ToLower();
+                    Console.WriteLine("Intentando acceder al  fichero:" + (line.Substring(2, line.Length - 2) + ".txt\n"));
+                    fichero = @"..\..\..\..\cuentos\" + line.Substring(2, line.Length - 2) + ".txt";
+                    
+                    if (!File.Exists(fichero))
+                    {
+                        Console.WriteLine("El fichero no existe");
+                        write.WriteLine("El fichero no existe");
+                        write.Flush();
+                    }
+
+                } while (!File.Exists(fichero));
+
+                str = new StreamReader(fichero);
+                Console.WriteLine("Fichero abierto. {0}\n", fichero);
+                write.WriteLine("Ficheros abierto.");
+                write.Flush();
+            }
+            catch (Exception e)
+                    {
+                        Console.WriteLine("Error: {0}  en la lectura de fichero", e.Message);
+                        Console.ReadLine();
+            }
+            return str;
+
+        }
+
+        //Método ProcesarLinea
+        //Descripción:Procesa las líneas que va leyendo del buffer del cuento.
+        //Envía al cliente las palabras a modificar, recepciona del buffer su contenido y guarda la línea en el fichero.
+        //@StreamReader readfich: Buffer de lectura del cuento
+        //@StreamWriter writebuffer: Buffer de escritura del pipe.
+        //@StreamReader readbuffer: Buffer de lectura del pipe.
+        //@String fichero: Nombre del fichero donde se debe guardar el cuento.
+        
+        private static void ProcesarLinea(StreamReader readfich, StreamWriter writebuffer, StreamReader readbuffer, String fichero)
+        {
+            String proclinea = String.Empty;
+            String linea = String.Empty;
+            StreamWriter fichsalida = StreamWriter.Null;
+            try
+            {
+                proclinea = readfich.ReadLine();
+                Console.WriteLine("Tubo Servidor procesando datos: '{0}'\n", proclinea);
+                fichsalida = new StreamWriter(fichero);
+
+                while (proclinea != null)
+                {
+
+
+                    while (proclinea.Contains("<") && proclinea.Contains(">"))
+                    {
+
+                        int indice1 = proclinea.IndexOf('<');
+                        int indice2 = proclinea.IndexOf('>');
+                        var texto = proclinea.Substring((indice1 + 1), ((indice2 - indice1) - 1));
+
+
+                        writebuffer.WriteLine("P " + proclinea.Substring((indice1 + 1), ((indice2 - indice1) - 1)));
+                        writebuffer.Flush();
+                        Console.WriteLine("Tubo Servidor emitiendo datos: '{0}'\n", proclinea.Substring((indice1 + 1), ((indice2 - indice1) - 1)));
+                        linea = readbuffer.ReadLine();
+                        Console.WriteLine("Tubo Servidor recibiendo datos: '{0}'\n", linea);
+
+
+                        var palabra1 = proclinea.Substring(indice1, ((indice2 - indice1) + 1));
+                        var palabra2 = linea.Substring(2, linea.Length - 2);
+                        proclinea = proclinea.Replace(palabra1, palabra2);
+
+
+                    }
+
+                    fichsalida.WriteLine(proclinea);
+
+                    proclinea = readfich.ReadLine();
+                    Console.WriteLine("Leyendo datos de fichero para procesar: '{0}'\n", proclinea);
+                    linea = "";
+                }
+                fichsalida.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  al procesar los datos", e.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        //Método EnviarCuento
+        //Descripción:Lee el cuento procesado desde el fichero y se lo envía al cliente para que lo muestre.
+        //@StreamWriter write: Buffer de escritura del pipe.
+        //@String fichero: Nombre del fichero donde se debe guardar el cuento.
+        private static void EnviarCuento(StreamWriter write, String fichero)
+        {
+            String proclinea = String.Empty;
+            try { 
+                var read = new StreamReader(fichero);
+                proclinea = read.ReadToEnd();
+
+
+                Console.WriteLine("Tubo Servidor emitiendo datos cuento.\n");
+                write.WriteLine(proclinea);
+                write.Flush();
+                read.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  en el envio de cuento", e.Message);
+                Console.ReadLine();
+            }
+}
+    }
+}      
+
